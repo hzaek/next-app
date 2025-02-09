1a39fe71fca3bdaaf4726f5fe5f40eb90f9d7b5b
Arch Laptop <xruby730@gmail.com>
2025-02-09 13:19:54 +0400

    te

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+// export default nextConfig;
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions:{
+    disableDevLogs: true,
+  }
+});
+module.exports = withPWA(nextConfig)
